edc03d9acc9d076ccb544fc4b217915c0dbeebd4
Alberto Zaranza <albertozaranza@outlook.com>
2022-05-17 22:44:35 -0300

    configure fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      secondary: ['Rubik', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        purple: "#6548A3",
+
+        background: "#F0EDF5",
+        input: "#C4C4D1",
+        detail: "#A0A0B2",
+        "base-darker": "#6C6C80",
+        title: "#3D3D4C",
+      },
+    },
   },
   plugins: [],
 }
